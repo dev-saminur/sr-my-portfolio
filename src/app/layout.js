ec8874665183f178ce50d141149d616f3b609d1f
Md. Saminur Islam <saminur9125@gmail.com>
2024-10-08 11:31:42 +0600

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,13 @@
 import "./globals.css";
 
+import { Open_Sans } from "next/font/google";
+
+const openSans = Open_Sans({
+  subsets: ["latin"], // you can add 'latin-ext' if needed
+  weight: ["300", "400", "600", "700"], // define the font weights you need
+  style: ["normal", "italic"], // normal or italic
+  variable: "--font-open-sans", // Optional: CSS variable for the font
+});
 export const metadata = {
   title: "SR Portfolio",
   description: "Created by SR RaKiB",
@@ -8,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className=" font-primaryFont">{children}</body>
+      <body className={openSans.className}>{children}</body>
     </html>
   );
 }
