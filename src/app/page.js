b4549f808d88f14f0bd51da4ca0a66a1b965d499
Md. Saminur Islam <saminur9125@gmail.com>
2024-10-08 12:14:57 +0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import About from "@/components/About";
 import Hero from "@/components/Hero";
 import Layout from "@/components/Layout/Layout";
 
@@ -7,6 +8,7 @@ export default function Home() {
       <div>
         <Layout>
           <Hero />
+          <About />
         </Layout>
       </div>
     </>
