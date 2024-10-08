e6dc5241ccb001f2e8f05f9c0a58b07d75947df4
Md. Saminur Islam <saminur9125@gmail.com>
2024-10-08 11:31:46 +0600

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>This is footer</div>;
+  return (
+    <>
+      <div className="bg-gray py-8">
+        <p className="text-base text-center text-whiteColor font-normal">
+          Copyright © 2024{" "}
+          <span className="text-linkColor font-medium">SR Rakib</span>. All
+          Rights Reserved.
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
