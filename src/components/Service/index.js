c5fe30969838700a29e437b37afce946b5e84a88
Md. Saminur Islam <saminur9125@gmail.com>
2024-10-09 21:28:09 +0600

    images-added

---
+++ src/components/Service/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Container from "../Container/Container";
+
+const Service = () => {
+  return (
+    <>
+      <div className="py-5">
+        <Container>
+          <div className="grid grid-cols-3 gap-8">
+            <div className=" py-12 px-9 rounded-xl  shadow-[0px_0px_10px_4px_#2E3D44]">
+              one
+            </div>
+            <div className="bg-slate-700">two</div>
+            <div className="bg-blue-300">three</div>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default Service;
