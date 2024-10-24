9658589552f485a1377166219831299dd413c195
Md. Saminur Islam <saminur9125@gmail.com>
2024-10-24 13:34:22 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,30 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        "name-gradient":
+          "linear-gradient(to right, #4fcf70, #fad648, #a767e5, #12bcfe, #44ce7b)",
+        "designation-gradient":
+          "linear-gradient(to right, #15BEC5, #C0FFFF, #fad648, #a767e5, #44ce7b)",
+        "pera-gradient":
+          "linear-gradient(to right, #12bcfe, #fad648, #15BEC5, #a767e5, #44ce7b)",
+      },
       keyframes: {
         borderMove: {
           "0%": { transform: "translateX(-5%)" },
           "100%": { transform: "translateX(-25%)" },
         },
+
+        gradientAnimate: {
+          "0%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+          "100%": { backgroundPosition: "0% 50%" },
+        },
       },
+
       animation: {
         borderMove: "borderMove 0.75s linear infinite",
+        gradientAnimate: "gradientAnimate 3s ease infinite",
       },
 
       screens: {
@@ -42,5 +58,5 @@ module.exports = {
       center: true,
     },
   },
-  plugins: [],
+  plugins: ["prettier-plugin-tailwindcss"],
 };
