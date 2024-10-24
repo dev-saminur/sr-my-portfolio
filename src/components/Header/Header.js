c4ec6d045ac12c46329cae004cc6f47c285d7b0e
Md. Saminur Islam <saminur9125@gmail.com>
2024-10-24 13:34:14 +0600

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const Header = () => {
+const Header = ({ title, className }) => {
   return (
     <>
+      <h1
+        className={`text-[60px] relative text-sectionColor font-bold capitalize leading-[1.2] before:absolute before:bottom-0 before:right-0 before:content-['']  before:w-[90%] before:h-[6px]  before:bg-gradient-to-tr from-linkColor to-headerColor  pb-4 inline-block before:rounded-t-lg  before:rounded-b-2xl before:rounded-l-3xl before:rounded-r-lg  ${className}`}
+      >
+        {title}
+      </h1>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
