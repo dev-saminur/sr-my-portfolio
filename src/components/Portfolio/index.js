479d122cde4544b575ecd9ad67a2936b10de03c5
Md. Saminur Islam <saminur9125@gmail.com>
2024-10-24 23:00:25 +0600

    Create index.js

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Container from "../Container/Container";
+
+const Portfolio = () => {
+  return (
+    <>
+      <div className="portfolio">
+        <Container>
+          <div>this is container</div>
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default Portfolio;
