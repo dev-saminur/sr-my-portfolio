37ce808d1d07761ec002498e65e85492307e81be
Md. Saminur Islam <saminur9125@gmail.com>
2024-10-24 14:06:13 +0600

    Create serviceData.js

---
+++ src/components/Service/serviceData.js
@@ -0,0 +1,35 @@
+import Coder from "../image/icon/design(5).png";
+import Webdevelopment from "../image/icon/design(6).png";
+import Webdesign from "../image/icon/design(2).png";
+export const serviceData = [
+  {
+    image: Coder,
+    title: "Mern Stack Development",
+    subtitle: "Create a platform with the best and coolest quality from us.",
+  },
+  {
+    image: Webdevelopment,
+    title: "Apps Development",
+    subtitle: "Create a platform with the best and coolest quality from us.",
+  },
+  {
+    image: Webdesign,
+    title: "Front-end Development",
+    subtitle: "Create a platform with the best and coolest quality from us.",
+  },
+  {
+    image: Webdevelopment,
+    title: "Landing page design",
+    subtitle: "Create a platform with the best and coolest quality from us.",
+  },
+  {
+    image: Webdesign,
+    title: "responsive Web design",
+    subtitle: "Create a platform with the best and coolest quality from us.",
+  },
+  {
+    image: Coder,
+    title: "figma to html expert ",
+    subtitle: "Create a platform with the best and coolest quality from us.",
+  },
+];
