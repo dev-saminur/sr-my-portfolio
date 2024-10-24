ad0100d9e33215980c5bed58a5ffe70756ab1fe0
Md. Saminur Islam <saminur9125@gmail.com>
2024-10-24 13:49:04 +0600

    Update index.js

--- src/components/Service/index.js
+++ src/components/Service/index.js
@@ -13,15 +13,15 @@ const Service = () => {
             <Header title="My services" />
           </div>
           <div className="grid grid-cols-3 gap-8">
-            <div className="group bg-gradient-to-tr  from-teal-400 via-blue-500 to-purple-500 rounded-xl p-[2px] hover:transition-all hover:duration-500 hover:ease-linear  hover:bg-gradient-to-r hover:from-blue-400 hover:via-purple-500 hover:to-teal-500 hover:animate-gradientAnimate">
+            <div className="group bg-gradient-to-tr  from-teal-400 via-blue-500 to-purple-500 rounded-xl p-[2px] hover:transition-all hover:duration-500 hover:ease-linear  hover:bg-gradient-to-r hover:from-blue-400 hover:via-purple-500 hover:to-teal-500 hover:animate-gradientAnimate delay-300	ease-linear">
               <div className="py-14 px-10 text-center bg-[#000807]  rounded-xl ">
-                <div className="mb-5 w-[76px] h-[76px] flex justify-center items-center bg-[#1F303E] group-hover:bg-gradient-to-tr group-hover:bg-opacity-5  from-teal-600 via-[#3b83f65d] to-[#a855f763 ] rounded-full mx-auto group-hover:transition-all group-hover:duration-300">
+                <div className="mb-5 w-[76px] h-[76px] flex justify-center items-center bg-[#1F303E] group-hover:bg-gradient-to-tr group-hover:bg-opacity-5  from-teal-600 via-[#3b83f65d] to-[#a855f763 ] rounded-full mx-auto group-hover:transition-all group-hover:duration-300 group-hover:ease-linear">
                   <Image src={Coding} width={32} height={32} alt="coding" />
                 </div>
-                <h4 className=" text-[28px] font-semibold text-linkColor mb-5 text-center group-hover:bg-name-gradient group-hover:text-transparent  group-hover:bg-clip-text group-hover:transition-all group-hover:duration-300">
+                <h4 className=" text-[28px] font-semibold text-linkColor mb-5 text-center group-hover:bg-name-gradient group-hover:text-transparent  group-hover:bg-clip-text group-hover:transition-all group-hover:duration-300 group-hover:ease-linear">
                   Mern Stack Development
                 </h4>
-                <p className="text-base font-normal leading-tight text-headerColor mb-5 text-center group-hover:bg-pera-gradient group-hover:text-transparent  group-hover:bg-clip-text group-hover:font-semibold group-hover:transition-all group-hover:duration-300">
+                <p className="text-base font-normal leading-tight text-headerColor mb-5 text-center group-hover:bg-pera-gradient group-hover:text-transparent  group-hover:bg-clip-text group-hover:font-semibold group-hover:transition-all group-hover:duration-300 group-hover:ease-linear	">
                   Create a platform with the best and coolest quality from us.
                 </p>
               </div>
