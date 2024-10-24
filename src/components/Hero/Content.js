50d045c22124cf148068aaf8d6d1efc1c64f6abe
Md. Saminur Islam <saminur9125@gmail.com>
2024-10-24 13:34:18 +0600

    Update Content.js

--- src/components/Hero/Content.js
+++ src/components/Hero/Content.js
@@ -4,9 +4,13 @@ import Buttons from "../Buttons/Button";
 const Content = () => {
   return (
     <div>
-      <h3 className="text-[32px] text-headerColor font-normal">Hi ! I’m </h3>
-      <h2 className="text-[64px] text-linkColor font-bold mb-1">Dev_Saminur</h2>
-      <h4 className="text-4xl text-headerColor font-normal">
+      <h3 className="text-[32px] font-normal text-transparent bg-clip-text bg-gradient-to-b from-yellow-400  to-linkColor">
+        Hi ! I’m{" "}
+      </h3>
+      <h2 className=" text-[64px]  font-bold mb-1 text-transparent  bg-clip-text bg-name-gradient ">
+        Md. Saminur Islam
+      </h2>
+      <h4 className="text-4xl font-semibold  bg-designation-gradient inline-block py-4 px-10">
         Front-end Developer
       </h4>
       <div className="mt-10">
