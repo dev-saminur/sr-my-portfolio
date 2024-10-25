c04aacf721d2a750dc49103d28d68bbd7933d789
Md. Saminur Islam <saminur9125@gmail.com>
2024-10-26 00:44:30 +0600

    Update index.js

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -11,13 +11,11 @@ const Portfolio = () => {
           <div className="text-center mb-10">
             <Header title="My portfolio" />
             <div className="grid grid-cols-12">
-              <div className="grid col-start-4 col-end-10 mt-12 mb-4">
-                <p className="text-[38px] px-10 text-center bg-name-gradient text-transparent  bg-clip-text font-bold tracking-widest">
-                  Some of my
-                  <span className="bg-designation-gradient text-transparent  bg-clip-text pl-2">
-                    Projects
-                  </span>
-                </p>
+              <div className="grid col-start-4 col-end-10 mt-12 mb-7">
+                <Header
+                  title="  Some of my projects"
+                  className="text-3xl mt-6   pointer before:w-[40px] before:h-[30px] rounded-full before:bottom-[-30%] before:right-[46%] leading-10 z-[999]"
+                />
               </div>
             </div>
           </div>
