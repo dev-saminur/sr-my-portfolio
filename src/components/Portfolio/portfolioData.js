d4ffdd637c14380e35b23810ea20dd1a0b489720
Md. Saminur Islam <saminur9125@gmail.com>
2024-10-26 01:07:59 +0600

    Create portfolioData.js

---
+++ src/components/Portfolio/portfolioData.js
@@ -0,0 +1,36 @@
+import PortImage from "../image/portfolio.png";
+import PortImage2 from "../image/chatting.png";
+import PortImage3 from "../image/iteck-solution.png";
+
+export const portfolioData = [
+  {
+    image: PortImage,
+    githublink: "https://github.com/dev-saminur/sass-portfolio",
+    livelink: "https://sass-portfolio-tan.vercel.app/",
+  },
+  {
+    image: PortImage2,
+    githublink: "https://github.com/dev-saminur/chit-chat",
+    livelink: "https://chit-chat-sandy.vercel.app/",
+  },
+  {
+    image: PortImage3,
+    githublink: "https://github.com/dev-saminur/iteck-solution",
+    livelink: "https://iteck-solutions.netlify.app/",
+  },
+  {
+    image: PortImage2,
+    githublink: "https://github.com/dev-saminur/chit-chat",
+    livelink: "https://chit-chat-sandy.vercel.app/",
+  },
+  {
+    image: PortImage3,
+    githublink: "https://github.com/dev-saminur/iteck-solution",
+    livelink: "https://iteck-solutions.netlify.app/",
+  },
+  {
+    image: PortImage,
+    githublink: "https://github.com/dev-saminur/chit-chat",
+    livelink: "https://chit-chat-sandy.vercel.app/",
+  },
+];
