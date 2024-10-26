1ae370cb4ea313867f25c956c493369011d53242
Md. Saminur Islam <saminur9125@gmail.com>
2024-10-26 23:32:22 +0600

    Update portfolioData.js

--- src/components/Portfolio/portfolioData.js
+++ src/components/Portfolio/portfolioData.js
@@ -5,31 +5,38 @@ import PortImage4 from "../image/enver.png";
 
 export const portfolioData = [
   {
+    title: " Natto personal portfolio with Next.js and sass full responsove",
     image: PortImage,
     githublink: "https://github.com/dev-saminur/sass-portfolio",
     livelink: "https://sass-portfolio-tan.vercel.app/",
   },
   {
+    title:
+      " my chatting applitaion with react js,  bootstrap and functional component ",
     image: PortImage2,
     githublink: "https://github.com/dev-saminur/chit-chat",
     livelink: "https://chit-chat-sandy.vercel.app/",
   },
   {
+    title: " blog website with next.js and tailwindcss full responsive",
     image: PortImage3,
     githublink: "https://github.com/dev-saminur/iteck-solution",
     livelink: "https://iteck-solutions.netlify.app/",
   },
   {
+    title: " Enver landing page with html, css and bootstrap full responsove",
     image: PortImage4,
     githublink: "https://github.com/dev-saminur/enver-website",
     livelink: "https://dev-saminur.github.io/enver-website/",
   },
   {
+    title: " Natto personal portfolio with Next.js and sass",
     image: PortImage3,
     githublink: "https://github.com/dev-saminur/iteck-solution",
     livelink: "https://iteck-solutions.netlify.app/",
   },
   {
+    title: " Natto personal portfolio with Next.js and sass",
     image: PortImage,
     githublink: "https://github.com/dev-saminur/chit-chat",
     livelink: "https://chit-chat-sandy.vercel.app/",
