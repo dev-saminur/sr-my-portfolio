759914d823d8443c899d7ae9eebb4d5a63ec39dd
Md. Saminur Islam <saminur9125@gmail.com>
2024-10-27 13:20:16 +0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero";
 import Layout from "@/components/Layout/Layout";
 import Portfolio from "@/components/Portfolio";
 import Service from "@/components/Service";
+import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <Layout>
           <Hero />
           <About />
+          <Skills />
           <Service />
           <Portfolio />
         </Layout>
