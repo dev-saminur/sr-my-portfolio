76169e93064bb50ffa45b77358f653b0b5befbb2
Md. Saminur Islam <saminur9125@gmail.com>
2024-10-27 22:11:11 +0600

    Update Button.js

--- src/components/Buttons/Button.js
+++ src/components/Buttons/Button.js
@@ -2,11 +2,11 @@ import React from "react";
 import Link from "next/link";
 import "./style.css";
 
-const Buttons = ({ title, className }) => {
+const Buttons = ({ title, className, href }) => {
   return (
     <>
       <Link
-        href="/"
+        href={href}
         className={`sr-button relative inline-block cursor-pointer tracking-wides text-headerColor uppercase mx-auto font-normal rounded-2xl overflow-hidden p-[3px] isolate ${className}`}
       >
         <span className="relative block px-7 py-2 text-lg bg-black   rounded-tl-2xl rounded-br-2xl">
