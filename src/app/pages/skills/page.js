cdcfc0a388355d15058e20a969eff2685b15ba6e
Md. Saminur Islam <saminur9125@gmail.com>
2024-10-27 22:11:53 +0600

    Create page.js

---
+++ src/app/pages/skills/page.js
@@ -0,0 +1,17 @@
+import Layout from "@/components/Layout/Layout";
+import Skills from "@/components/Skills";
+import React from "react";
+
+const SkillsPage = () => {
+  return (
+    <>
+      <Layout>
+        <div className="pt-20 ">
+          <Skills />
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+export default SkillsPage;
