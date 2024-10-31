bdb69c2411a1fe7dc4a94cab3a77b3c69b346682
Md. Saminur Islam <saminur9125@gmail.com>
2024-10-31 22:31:45 +0600

    Update index.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -11,10 +11,12 @@ const Contact = () => {
         <Container>
           <div>
             <div className="text-center">
-              <Header title="contact me" />
-              <h4 className="text-5xl text-linkColor font-bold tracking-widest my-10">
-                Let&apos;s Work Together
-              </h4>
+              <Header title="contact me" className="text-center" />
+            </div>
+            <div className="my-10 text-center">
+              <p className="relative inline-block text-[38px] capitalize text-center bg-name-gradient text-transparent  bg-clip-text font-bold tracking-widest pb-6 after:absolute after:bottom-0 after:left-0 after:content-[' '] after:w-full after:h-[6px] after:bg-designation-gradient before:absolute before:bottom-[-10px] before:z-10 before:right-[50%] before:left-[50%] before:content-[' '] before:w-[30px] before:h-[30px] before:bg-name-gradient before:rounded-full before:overflow-hidden">
+                Let&apos;s Work Together!
+              </p>
             </div>
           </div>
           <div className="absolute bottom-[-45%] scale-50 lg:scale-100 left-[6%] lg:bottom-[-33%] lg:left-[50%] xl:bottom-[-450px]  xl:left-[60%] lg:translate-x-[-50%] ">
