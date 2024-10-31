67644549b07ca2ec14a2e20a7056659e29c4ed6b
Md. Saminur Islam <saminur9125@gmail.com>
2024-10-31 21:27:33 +0600

    Update index.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,11 +7,14 @@ import ContactForm from "./ContactForm";
 const Contact = () => {
   return (
     <>
-      <div className=" pt-1 pb-[150px] lg:pb-[250px] relative overflow-hidden">
+      <div className=" pt-1 pb-[122px]  relative overflow-hidden">
         <Container>
           <div>
             <div className="text-center">
               <Header title="contact me" />
+              <h4 className="text-5xl text-linkColor font-bold tracking-widest my-10">
+                Let&apos;s Work Together
+              </h4>
             </div>
           </div>
           <div className="absolute bottom-[-45%] scale-50 lg:scale-100 left-[6%] lg:bottom-[-33%] lg:left-[50%] xl:bottom-[-450px]  xl:left-[60%] lg:translate-x-[-50%] ">
