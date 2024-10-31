1c4c377b80ad5b2d7e02a0fb75d02d2358794797
Md. Saminur Islam <saminur9125@gmail.com>
2024-10-31 22:31:20 +0600

    Update Button.js

--- src/components/Buttons/Button.js
+++ src/components/Buttons/Button.js
@@ -7,9 +7,9 @@ const Buttons = ({ title, className, href }) => {
     <>
       <Link
         href={href}
-        className={`sr-button relative inline-block cursor-pointer tracking-wides text-headerColor uppercase mx-auto font-normal rounded-2xl overflow-hidden p-[3px] isolate ${className}`}
+        className={`sr-button relative inline-block cursor-pointer tracking-wides text-headerColor uppercase mx-auto font-normal rounded-full overflow-hidden p-[3px] isolate ${className}`}
       >
-        <span className="relative block px-7 py-2 text-lg bg-black   rounded-tl-2xl rounded-br-2xl">
+        <span className="relative block px-7 py-2 text-lg bg-black   rounded-full">
           {title}
         </span>
       </Link>
