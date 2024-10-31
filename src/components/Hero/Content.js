29af7677f1fb794f224eb748ec35ee46bb6fc8b3
Md. Saminur Islam <saminur9125@gmail.com>
2024-10-31 22:31:51 +0600

    Update Content.js

--- src/components/Hero/Content.js
+++ src/components/Hero/Content.js
@@ -6,13 +6,13 @@ import { Typewriter } from "react-simple-typewriter";
 const Content = () => {
   return (
     <div>
-      <h3 className="text-[32px] font-semibold text-transparent bg-clip-text bg-gradient-to-t to-[rgb(250,72,205)] via-[#78fe12] from-[#44ce7b]">
+      <h3 className="text-3xl font-semibold text-transparent  bg-clip-text bg-pera-gradient ">
         Hi ! I’m{" "}
       </h3>
-      <h2 className=" text-[64px]  font-bold mb-1 text-transparent  bg-clip-text bg-designation-gradient ">
+      <h2 className=" text-[64px]  font-bold mb-1 text-transparent  bg-clip-text bg-name-gradient ">
         Md. Saminur Islam
       </h2>
-      <h4 className="text-4xl font-semibold  bg-name-gradient inline-block py-4 px-10 capitalize">
+      <h4 className="text-4xl font-semibold  bg-pera-gradient inline-block py-4 px-10 capitalize">
         <Typewriter
           words={[
             "Front-end developer",
