da2cb8bf5e697aa2719df0f17ffda517c88a76f3
Md. Saminur Islam <saminur9125@gmail.com>
2024-11-02 21:47:28 +0600

    Update Container.js

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,9 +1,7 @@
 import React from "react";
 
 const Container = ({ children, className }) => {
-  return (
-    <div className={`container px-3 md:px-0 ${className}`}>{children}</div>
-  );
+  return <div className={`container px-3  ${className}`}>{children}</div>;
 };
 
 export default Container;
