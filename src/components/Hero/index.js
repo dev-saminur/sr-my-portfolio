736f31dc43d8d55458bcfceff48d8d53feea6bff
Md. Saminur Islam <saminur9125@gmail.com>
2024-11-02 13:07:37 +0600

    Update index.js

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -8,7 +8,7 @@ const Hero = () => {
   return (
     <div className="bg-black">
       <Container>
-        <div className="grid grid-cols-2 items-center">
+        <div className="grid grid-cols-1 lg:grid-cols-2 items-center">
           <div>
             <Content />
           </div>
