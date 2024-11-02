4adbbcd024adaf92396d99fc7785bfbe89d5e3d3
Md. Saminur Islam <saminur9125@gmail.com>
2024-11-02 14:14:06 +0600

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ const Header = ({ title, className }) => {
   return (
     <>
       <h1
-        className={`text-[60px] text-transparent bg-clip-text bg-designation-gradient opacity-50  relative font-bold capitalize leading-[1.2] before:absolute before:bottom-0 before:right-0 before:content-['']  before:w-[90%] before:h-[6px]  before:bg-name-gradient  pb-4 inline-block before:rounded-t-lg  before:rounded-b-2xl before:rounded-l-3xl before:rounded-r-lg  ${className}`}
+        className={`text-5xl lg:text-[60px] text-transparent bg-clip-text bg-designation-gradient opacity-50  relative font-bold capitalize leading-[1.2] before:absolute before:bottom-0 before:right-0 before:content-['']  before:w-[90%] before:h-[6px]  before:bg-name-gradient  pb-4 inline-block before:rounded-t-lg  before:rounded-b-2xl before:rounded-l-3xl before:rounded-r-lg  ${className}`}
       >
         {title}
       </h1>
