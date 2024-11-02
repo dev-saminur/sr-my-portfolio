d4b12cd6987b09959fbee7f36f74214a408fd8d4
Md. Saminur Islam <saminur9125@gmail.com>
2024-11-02 14:14:02 +0600

    Update index.js

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,36 +8,28 @@ import Header from "../Header/Header";
 const About = () => {
   return (
     <>
-      <div className="py-20">
+      <div className=" pb-0 lg:py-20">
         <Container>
-          <div className="grid grid-cols-2 items-center">
+          <div className="flex flex-col-reverse lg:grid lg:grid-cols-2 items-center">
             <div className="left ">
-              <div className="w-[550px] h-[700px] bg-red-400 relative ">
-                <Image
-                  src={AboutBg}
-                  width={550}
-                  height={700}
-                  alt="about-bg"
-                  className="absolute top-0 left-0 h-full"
-                />
-              </div>
+              <Image src={AboutBg} width={550} height={700} alt="about-bg" />
             </div>
-            <div className="left">
+            <div className="right text-center lg:text-start">
               <Header title=" About me!" className="mb-6" />
-              <p className="text-headerColor opacity-70 text-[17px] tracking-wider font-normal mb-4 text-justify">
+              <p className="text-headerColor opacity-70 text-sm lg:text-[17px] tracking-wider font-normal mb-4 text-justify">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum in
                 eos saepe ipsa cupiditate accusantium voluptatibus quidem nam,
                 reprehenderit, et necessitatibus adipisci labore sit veritatis
                 vero tempore sequi at sed facere dolore. Quae obcaecati eius
                 quasi doloribus illum minus fugit.
               </p>
-              <p className="text-headerColor opacity-70 text-[17px] tracking-wider font-normal text-justify">
+              <p className="text-headerColor opacity-70 text-sm lg:text-[17px] tracking-wider font-normal text-justify">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum in
                 eos saepe ipsa cupiditate accusantium voluptatibus quidem nam,
                 reprehenderit,
               </p>
 
-              <div className="mt-8">
+              <div className="mt-8 mb-10 lg:mb-0">
                 <Buttons
                   href="/cv/cv.pdf"
                   download="cv.pdf"
