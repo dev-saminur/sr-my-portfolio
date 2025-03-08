d030abb533479c7c9fbeaac679b043d4489c3800
Md. Saminur Islam <saminur9125@gmail.com>
2025-03-08 22:53:14 +0600

    Update Button.js

--- src/components/Buttons/Button.js
+++ src/components/Buttons/Button.js
@@ -8,6 +8,7 @@ const Buttons = ({ title, className, href }) => {
       <Link
         href={href}
           target="_blank"
+          passHref 
         className={`sr-button relative inline-block cursor-pointer tracking-wides text-headerColor uppercase mx-auto font-normal rounded-full overflow-hidden p-[3px] isolate ${className}`}
       >
         <span className="relative block px-7 lg:px-3 xl:px-7 py-2 text-base xl:text-lg bg-black   rounded-full">
